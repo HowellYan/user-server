b65a4ffc180be3583dd5ce2ed8b196f7bf75ef27
howell <th15817161961@gmail.com>
2017-07-23 01:48:47 +0800

    v1.0.0

---
+++ user-impl/src/main/java/cn/com/alien/user/settings/myBatis/MyBatisConfig.java
@@ -0,0 +1,61 @@
+package cn.com.alien.user.settings.myBatis;
+
+import cn.com.alien.user.settings.properties.MyBatisProperties;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+/**
+ * Created by Howell on 13/12/16.
+ */
+
+@Configuration
+@Slf4j
+public class MyBatisConfig {
+
+    @Autowired
+    MyBatisProperties myBatisProperties;
+
+    @Bean
+    public DataSource getDataSource() throws Exception{
+        Properties props = new Properties();
+        props.put("driverClassName", myBatisProperties.getDriver());
+        props.put("url", myBatisProperties.getUrl());
+        props.put("username", myBatisProperties.getUsername());
+        props.put("password", myBatisProperties.getPassword());
+        props.put("maxActive", myBatisProperties.getMaxActive());
+        props.put("minIdle", myBatisProperties.getMinIdle());
+        props.put("maxWait", myBatisProperties.getMaxWait());
+        props.put("timeBetweenEvictionRunsMillis", myBatisProperties.getTimeBetweenEvictionRunsMillis());
+        props.put("testWhileIdle", myBatisProperties.getTestWhileIdle());
+        props.put("testOnBorrow", myBatisProperties.getTestOnBorrow());
+        props.put("testOnReturn", myBatisProperties.getTestOnReturn());
+        return DruidDataSourceFactory.createDataSource(props);
+    }
+
+    /**
+     *  根据数据源创建SqlSessionFactory
+     */
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource ds) throws Exception{
+        SqlSessionFactoryBean fb = new SqlSessionFactoryBean();
+
+        fb.setDataSource(ds);//指定数据源(这个必须有，否则报错)
+        //下边两句仅仅用于*.xml文件，如果整个持久层操作不需要使用到xml文件的话（只用注解就可以搞定），则不加
+        //fb.setTypeAliasesPackage("cn.com.ratpack");//指定基包
+        fb.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));//指定xml文件位置
+        return fb.getObject();
+    }
+
+
+
+
+}
---
+++ user-impl/src/main/java/cn/com/alien/user/settings/properties/MyBatisProperties.java
@@ -0,0 +1,108 @@
+package cn.com.alien.user.settings.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "myBatis")
+public class MyBatisProperties {
+    private String driver;
+    private String url;
+    private String username;
+    private String password;
+    private String maxActive;
+    private String minIdle;
+    private String maxWait;
+    private String timeBetweenEvictionRunsMillis;
+    private String testWhileIdle;
+    private String testOnBorrow;
+    private String testOnReturn;
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMaxActive() {
+        return maxActive;
+    }
+
+    public void setMaxActive(String maxActive) {
+        this.maxActive = maxActive;
+    }
+
+    public String getMinIdle() {
+        return minIdle;
+    }
+
+    public void setMinIdle(String minIdle) {
+        this.minIdle = minIdle;
+    }
+
+    public String getMaxWait() {
+        return maxWait;
+    }
+
+    public void setMaxWait(String maxWait) {
+        this.maxWait = maxWait;
+    }
+
+    public String getTimeBetweenEvictionRunsMillis() {
+        return timeBetweenEvictionRunsMillis;
+    }
+
+    public void setTimeBetweenEvictionRunsMillis(String timeBetweenEvictionRunsMillis) {
+        this.timeBetweenEvictionRunsMillis = timeBetweenEvictionRunsMillis;
+    }
+
+    public String getTestWhileIdle() {
+        return testWhileIdle;
+    }
+
+    public void setTestWhileIdle(String testWhileIdle) {
+        this.testWhileIdle = testWhileIdle;
+    }
+
+    public String getTestOnBorrow() {
+        return testOnBorrow;
+    }
+
+    public void setTestOnBorrow(String testOnBorrow) {
+        this.testOnBorrow = testOnBorrow;
+    }
+
+    public String getTestOnReturn() {
+        return testOnReturn;
+    }
+
+    public void setTestOnReturn(String testOnReturn) {
+        this.testOnReturn = testOnReturn;
+    }
+}
